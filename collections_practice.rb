33d50ee2d11f055ccb4436ea6638fd692ea3fd39
fitzgebt <fitzgebt@gmail.com>
2021-01-15 14:00:07 -0800

    commit tree e6b0f92790da7dc73ad5d423e43723e5f194ee39

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,67 @@
+require 'pry'
 
+def sort_array_asc(array)
+    array.sort 
+end
+
+def sort_array_desc(array)
+    array.sort do |a, b|
+        if a == b
+            0
+        elsif a > b
+            -1
+        else a < b
+            1
+        end
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort do |a, b|
+        if a.length == b.length
+            0
+        elsif a.length < b.length
+             -1
+        else a.length > b.length
+            1
+        end
+    end
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each do |name|
+        name[2..2]="$"
+    end
+    return array
+end
+
+def find_a(array)
+    array.select{|word| word[0] == "a"}
+end
+
+def sum_array(array)
+    array.sum
+end
+
+def add_s(array)
+    new_array=[] 
+    i = 0
+    while i < array.length
+        if i == 1
+            new_array << array[i]
+        else
+            new_array << array[i] + "s"
+        end
+        i+=1
+    end
+    new_array
+end
